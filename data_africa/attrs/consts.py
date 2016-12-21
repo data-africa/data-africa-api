b323318a60738a8f03200cec96e5758c28fb3ac9
Jonathan Speiser <jspeis@gmail.com>
2016-12-21 09:28:46 -0500

    add overall category for water_supply

--- data_africa/attrs/consts.py
+++ data_africa/attrs/consts.py
@@ -1,8 +1,11 @@
+'''Constant string literals used throughout the API'''
+
 ADM0 = 'adm0'
 ADM1 = 'adm1'
 
 COUNTRY = 'country'
 ALL = 'all'
+OVERALL = 'overall'
 OR = ","
 YEAR = 'year'
 LATEST = 'latest'
