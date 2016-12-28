92c654e2cf6aa1281556b1740d8c946398769033
Jonathan Speiser <jspeis@gmail.com>
2016-12-28 15:34:24 -0500

    adding poverty by gender table

--- data_africa/poverty/models.py
+++ data_africa/poverty/models.py
@@ -1,7 +1,7 @@
 from data_africa.database import db
 from data_africa.core.models import BaseModel
 from data_africa.attrs.consts import ALL, ADM0, ADM1, IRR, RFD, OVERALL
-from data_africa.attrs.consts import LATEST_BY_GEO
+from data_africa.attrs.consts import LATEST_BY_GEO, GENDER
 from data_africa.spatial.models import PovertyXWalk
 from data_africa.attrs.models import Geo
 from sqlalchemy.orm import column_property
@@ -19,6 +19,13 @@ class BasePoverty(db.Model, BaseModel):
     source_title = 'Poverty Survey'
     source_link = 'http://www.harvestchoice.org/'
     source_org = 'IFPRI'
+
+    sevpov_ppp1 = db.Column(db.Float)
+    sevpov_ppp2 = db.Column(db.Float)
+    povgap_ppp1 = db.Column(db.Float)
+    povgap_ppp2 = db.Column(db.Float)
+    hc_poor1 = db.Column(db.Float)
+    hc_poor2 = db.Column(db.Float)
 
     @classmethod
     def geo_filter(cls, level):
@@ -46,6 +53,12 @@ class BasePoverty(db.Model, BaseModel):
             "geo": [ALL, ADM0, ADM1],
         }
 
+    @classmethod
+    def crosswalk(cls):
+        cond = PovertyXWalk.poverty_geo == cls.poverty_geo
+        involved_tables = (PovertyXWalk, cls)
+        return [involved_tables, cond]
+
 class Survey(BasePoverty):
     __tablename__ = "survey"
     median_moe = 0
@@ -53,24 +66,22 @@ class Survey(BasePoverty):
     year = db.Column(db.Integer(), primary_key=True)
     poverty_geo = db.Column(db.String(), primary_key=True)
     geo = column_property(PovertyXWalk.geo)
-    sevpov_ppp1 = db.Column(db.Float)
-    sevpov_ppp2 = db.Column(db.Float)
-    povgap_ppp1 = db.Column(db.Float)
-    povgap_ppp2 = db.Column(db.Float)
+
     gini = db.Column(db.Float)
-    hc_poor1 = db.Column(db.Float)
-    hc_poor2 = db.Column(db.Float)
-
-    @staticmethod
-    def crosswalk():
-        cond = PovertyXWalk.poverty_geo == Survey.poverty_geo
-        involved_tables = (PovertyXWalk, Survey)
-        return [involved_tables, cond]
-        # qry = qry.join(aliased_xwalk, aliased_xwalk.poverty_geo == Survey.poverty_geo)
-        # if "poverty_geo" in api_obj.vars_and_vals:
-            # pov_geos = api_obj.vars_and_vals["poverty_geo"].split(",")
-            # qry = qry.filter(or_(PovertyXWalk.geo.in_(pov_geos), PovertyXWalk.poverty_geo.in_(pov_geos)))
-        # return qry
 
 
-poverty_models = [Survey]
+class SurveyByGender(BasePoverty):
+    __tablename__ = "survey_by_gender"
+    median_moe = 2
+
+    year = db.Column(db.Integer(), primary_key=True)
+    gender = db.Column(db.String(), primary_key=True)
+    poverty_geo = db.Column(db.String(), primary_key=True)
+    geo = column_property(PovertyXWalk.geo)
+
+    @classmethod
+    def get_supported_levels(cls):
+        base_levels = super(SurveyByGender, cls).get_supported_levels()
+        return dict(base_levels, **{GENDER: ALL})
+
+poverty_models = [Survey, SurveyByGender]
