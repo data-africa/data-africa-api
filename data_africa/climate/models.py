9a01047cbd2bdffc58681ade7f2e0717bdf344d6
Jonathan Speiser <jspeis@gmail.com>
2017-01-05 14:51:31 -0500

    connecting climate rainfall data

---
+++ data_africa/climate/models.py
@@ -0,0 +1,44 @@
+from data_africa.database import db
+from data_africa.core.models import BaseModel
+from data_africa.attrs.consts import ALL, ADM0, ADM1
+from data_africa.attrs.consts import LATEST_BY_GEO
+
+from sqlalchemy import tuple_
+from sqlalchemy.sql import func
+
+
+class BaseClimate(db.Model, BaseModel):
+    __abstract__ = True
+    __table_args__ = {"schema": "climate", "extend_existing": True}
+    source_title = 'Climate Data'
+    source_link = 'http://www.harvestchoice.org/'
+    source_org = 'IFPRI'
+
+    @classmethod
+    def geo_filter(cls, level):
+        if level == ALL:
+            return True
+        elif level == ADM0:
+            return cls.geo.startswith("040")
+        elif level == ADM1:
+            return cls.geo.startswith("050")
+
+    @classmethod
+    def get_supported_levels(cls):
+        return {
+            "year": [ALL, LATEST_BY_GEO],
+            "geo": [ALL, ADM0, ADM1],
+        }
+
+
+class Rainfall(BaseClimate):
+    __tablename__ = "rainfall"
+    median_moe = 0
+
+    geo = db.Column(db.String(), primary_key=True)
+
+    rainfall_awa_mm = db.Column(db.Float)
+    cropland_rainfallCVgt20pct_pct = db.Column(db.Float)
+    cropland_rainfallCVgt30pct_ha = db.Column(db.Float)
+
+climate_models = [Rainfall]
--- data_africa/core/registrar.py
+++ data_africa/core/registrar.py
@@ -2,9 +2,11 @@ from data_africa.cell5m.models import cell5m_models
 from data_africa.poverty.models import poverty_models
 from data_africa.spatial.models import PovertyXWalk
 from data_africa.dhs.models import dhs_models
+from data_africa.climate.models import climate_models
 
 registered_models = [PovertyXWalk]
-registered_models += cell5m_models + poverty_models + dhs_models
+registered_models += (cell5m_models + poverty_models +
+                      dhs_models + climate_models)
 
 def register(cls):
     registered_models.append(cls)
