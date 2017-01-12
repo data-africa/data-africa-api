8a7a8269f4719bf79d0d8c82ac2c22713063789e
Jonathan Speiser <jspeis@gmail.com>
2017-01-12 13:38:15 -0500

    flake8 style cleanup

--- data_africa/core/views.py
+++ data_africa/core/views.py
@@ -3,7 +3,6 @@ from flask import Blueprint, request, jsonify
 from data_africa.core import table_manager
 from data_africa.core import join_api, api
 from data_africa.core.models import ApiObject
-from data_africa.core.crosswalker import crosswalk
 from data_africa.core.exceptions import DataAfricaException
 
 
@@ -11,10 +10,12 @@ mod = Blueprint('core', __name__, url_prefix='/api')
 
 manager = table_manager.TableManager()
 
+
 def show_attrs(attr_obj):
     attrs = attr_obj.query.all()
     data = [a.serialize() for a in attrs]
     return jsonify(data=data)
+
 
 def build_api_obj(default_limit=None):
     show = request.args.get("show", "")
@@ -39,12 +40,11 @@ def build_api_obj(default_limit=None):
         sumlevels = ["all" for show in shows]
     values = required.split(",") if required else []
 
-    shows_and_levels = {val:sumlevels[idx] for idx, val in enumerate(shows)}
+    shows_and_levels = {val: sumlevels[idx] for idx, val in enumerate(shows)}
 
     variables = manager.possible_variables
-    vars_and_vals = {var:request.args.get(var, None) for var in variables}
-    vars_and_vals = {k:v for k,v in vars_and_vals.items() if v}
-
+    vars_and_vals = {var: request.args.get(var, None) for var in variables}
+    vars_and_vals = {k: v for k, v in vars_and_vals.items() if v}
 
     vars_needed = list(vars_and_vals.keys()) + shows + values
     api_obj = ApiObject(vars_needed=vars_needed, vars_and_vals=vars_and_vals,
@@ -55,6 +55,7 @@ def build_api_obj(default_limit=None):
                         display_names=display_names,
                         offset=offset, inside=inside)
     return api_obj
+
 
 @mod.route("/")
 @mod.route("/v1/")
@@ -67,6 +68,7 @@ def api_view(csv=None):
     api_obj = manager.crosswalk(table, api_obj)
     data = api.query(table, api_obj, stream=csv)
     return data
+
 
 @mod.route("/join/")
 @mod.route("/join/csv/", defaults={'csv': True})
@@ -86,6 +88,7 @@ def logic_view():
     table_list = manager.all_tables(api_obj)
     return jsonify(tables=[table.info(api_obj) for table in table_list])
 
+
 @mod.route("/variables/")
 def view_variables():
     '''show available data tables and contained variables'''
@@ -95,16 +98,21 @@ def view_variables():
     if sumlevels == [""]:
         sumlevels = ["all"] * len(shows)
     combos = zip(shows, sumlevels)
-    results = {table.full_name(): table.col_strs(short_name=True) for table in table_manager.registered_models
-               if list_all or all([table.can_show(show, sumlevel) for show,sumlevel in combos])}
+    results = {}
+    for show, sumlevel in combos:
+        for table in table_manager.registered_models:
+            if list_all or all([table.can_show(show, sumlevel)]):
+                results[table.full_name()] = table.col_strs(short_name=True)
     return jsonify(metadata=results)
 
 
 @mod.route('/table/variables/')
 def all_table_vars():
     '''show all available data tables and contained variables'''
-    results = {table.full_name(): table.col_strs(short_name=True) for table in table_manager.registered_models}
+    results = {table.full_name(): table.col_strs(short_name=True)
+               for table in table_manager.registered_models}
     return jsonify(metadata=results)
+
 
 @mod.route("/years/")
 def years_view():
