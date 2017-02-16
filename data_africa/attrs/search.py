25f9e917b0b695c978ae2d437e6d01c6395307b9
Jonathan Speiser <jspeis@gmail.com>
2017-02-16 12:44:00 -0500

    expanding search to include adm1

--- data_africa/attrs/search.py
+++ data_africa/attrs/search.py
@@ -1,17 +1,20 @@
 from data_africa.attrs.models import Geo
-from sqlalchemy import func
+from sqlalchemy import func, or_
 
 
 def query(query_args):
     q = query_args.get('q', '')
     limit = int(query_args.get('limit', 10))
     offset = int(query_args.get('offset', 0))
-    # sumlevel = query_args.get('sumlevel', None)
+    sumlevel = query_args.get('sumlevel', None)
 
     focus_countries = ["040AF00094", "040AF00253", "040AF00170", "040AF00217", "040AF00042", "040AF00152", "040AF00270", "040AF00257", "040AF00079", "040AF00205", "040AF00182", "040AF00133"];
-
+    adm0s = [int(country[-3:]) for country in focus_countries]
     qry = Geo.query.filter(Geo.name.ilike("%{}%".format(q)))
-    qry = qry.filter(Geo.id.in_(focus_countries))
-    qry = qry.order_by(func.levenshtein(Geo.name, q))
+    cond = or_(Geo.id.in_(focus_countries), Geo.adm0_id.in_(adm0s))
+    qry = qry.filter(cond)
+    if sumlevel:
+        qry = qry.filter(Geo.level == sumlevel)
+    qry = qry.order_by(Geo.level, func.levenshtein(Geo.name, q))
     qry = qry.limit(limit).offset(offset)
     return qry.all()
