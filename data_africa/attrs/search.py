c5c34dea0dac472ae805eb6935c2e8d0389fcfbf
Jonathan Speiser <jspeis@gmail.com>
2017-02-28 14:26:11 -0500

    include mali

--- data_africa/attrs/search.py
+++ data_africa/attrs/search.py
@@ -8,7 +8,7 @@ def query(query_args):
     offset = int(query_args.get('offset', 0))
     sumlevel = query_args.get('sumlevel', None)
     excluded = ['050AF0017065246', '050AF0015265268']
-    focus_countries = ["040AF00094", "040AF00253", "040AF00170", "040AF00217", "040AF00042", "040AF00152", "040AF00270", "040AF00257", "040AF00079", "040AF00205", "040AF00182", "040AF00133"];
+    focus_countries = ["040AF00155", "040AF00094", "040AF00253", "040AF00170", "040AF00217", "040AF00042", "040AF00152", "040AF00270", "040AF00257", "040AF00079", "040AF00205", "040AF00182", "040AF00133"];
     adm0s = [int(country[-3:]) for country in focus_countries]
     qry = Geo.query.filter(Geo.name.ilike("%{}%".format(q)))
     cond = or_(Geo.id.in_(focus_countries), Geo.adm0_id.in_(adm0s))
